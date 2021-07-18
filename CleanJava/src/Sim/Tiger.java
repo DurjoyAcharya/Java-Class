dbcf1ae834b3c783a81a00ed65813c653477867c
Durjoy <da-durjoy@outlook.com>
2021-07-18 22:32:47 +0600

    some command line added

--- CleanJava/src/Sim/Tiger.java
+++ CleanJava/src/Sim/Tiger.java
@@ -1,7 +1,8 @@
-package CleanJava.src.Sim;
+package Sim;
 
 public class Tiger {
     public static void main(String[] args) {
         System.out.println("TIger Added");
+        System.out.println("Fucking pussy by Eclipse");
     }
 }
