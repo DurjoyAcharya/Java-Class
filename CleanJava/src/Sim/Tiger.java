6051280087075707bc10f1c89883f7b5573e7231
Durjoy <da-durjoy@outlook.com>
2021-07-18 22:40:30 +0600

    Somthing added

--- CleanJava/src/Sim/Tiger.java
+++ CleanJava/src/Sim/Tiger.java
@@ -1,8 +1,19 @@
-package Sim;
+package CleanJava.src.Sim;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Tiger {
+
     public static void main(String[] args) {
+        var s=List.of(new book("Java"),new book("Python"),new book("C++"),new book("C#"),
+        new book("R"),new book("JavaScript"),new book("Spring"));
         System.out.println("TIger Added");
         System.out.println("Fucking pussy by Eclipse");
+        s.stream().forEach(e->{
+            System.out.println(e);
+        });
     }
+    record book(String name){}
+
 }
